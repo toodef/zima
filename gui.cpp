d972a962c9ca562ddc3ba4e9bd320de59fe35e2b
Anton Fedotov <fedotov.anton.af@gmail.com>
2017-05-12 11:42:17 +0300

    opengl sample

--- gui.cpp
+++ gui.cpp
@@ -5,18 +5,17 @@ gui_t::gui_t( QWidget * parent ) : QMainWindow(parent)
 {
    setWindowTitle("Tif Viewer");
 
-   resize(1000, 600);
+   resize(1000, 700);
 
    QWidget * central_widget = new QWidget;
    QVBoxLayout * central_layout = new QVBoxLayout;
 
-   gl_widget_ = new QGLWidget();
-   central_layout->addWidget(gl_widget_);
+   renderer_ = new renderer_t();
+   central_layout->addWidget(renderer_);
 
    central_widget->setLayout(central_layout);
    setCentralWidget(central_widget);
 
-   renderer_ = new renderer_t(this);
 }
 
 gui_t::~gui_t()
--- renderer/renderer.hpp
+++ renderer/renderer.hpp
@@ -12,7 +12,42 @@ class renderer_t : public QGLWidget
 public:
    renderer_t( QWidget * parent = 0 );
 
-   void * display();
+private slots:
+   void advanceGears();
+
+protected:
+   void initializeGL();
+   void paintGL();
+   void resizeGL(int width, int height);
+   void mousePressEvent(QMouseEvent *event);
+   void mouseMoveEvent(QMouseEvent *event);
+
+public slots:
+   void setXRotation(int angle);
+   void setYRotation(int angle);
+   void setZRotation(int angle);
+
+signals:
+   void xRotationChanged(int angle);
+   void yRotationChanged(int angle);
+   void zRotationChanged(int angle);
+
 private:
-   QOpenGLContext * gl_context_;
+   //tmp
+   GLuint makeGear(const GLfloat *reflectance, GLdouble innerRadius,
+                               GLdouble outerRadius, GLdouble thickness,
+                               GLdouble toothSize, GLint toothCount);
+   void normalizeAngle(int *angle);
+   void drawGear(GLuint gear, GLdouble dx, GLdouble dy, GLdouble dz,
+                 GLdouble angle);
+
+   GLuint gear1;
+   GLuint gear2;
+   GLuint gear3;
+   int xRot;
+   int yRot;
+   int zRot;
+   int gear1Rot;
+
+   QPoint lastPos;
 };
