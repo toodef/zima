a203e95b3817bac179c3516aca30cc2d89a27e9e
Anton Fedotov <fedotov.anton.af@gmail.com>
2017-05-12 15:25:01 +0300

    add texture by shaders example

--- gui.cpp
+++ gui.cpp
@@ -5,7 +5,7 @@ gui_t::gui_t( QWidget * parent ) : QMainWindow(parent)
 {
    setWindowTitle("Tif Viewer");
 
-   resize(1000, 700);
+   resize(1000, 600);
 
    QWidget * central_widget = new QWidget;
    QVBoxLayout * central_layout = new QVBoxLayout;
