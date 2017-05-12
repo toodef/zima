424d68c0f42660a4cf43937452044c51d1ef2dc0
Anton Fedotov <fedotov.anton.af@gmail.com>
2017-05-12 08:53:45 +0300

    add renderer

---
+++ renderer/renderer.cpp
@@ -0,0 +1,5 @@
+//
+// Created by anton on 11-May-17.
+//
+
+#include "renderer.hpp"
---
+++ renderer/renderer.hpp
@@ -0,0 +1,15 @@
+//
+// Created by anton on 11-May-17.
+//
+
+#ifndef TIF_VIEWER_RENDERER_HPP
+#define TIF_VIEWER_RENDERER_HPP
+
+
+class renderer
+{
+
+};
+
+
+#endif //TIF_VIEWER_RENDERER_HPP
